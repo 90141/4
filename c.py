d38e952d2aac0314a9f39129c4fcfcbf5dadd39f
90141 <70868321+90141@users.noreply.github.com>
2020-09-21 09:11:44 +0800

    Add files via upload

---
+++ c.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+"""
+
+string='I love you'
+s1=string[0:1]
+s2=string[2:6]
+s3=string[7:10]
+print(s1,s2,s3)
+
+
+"""
+"""
+
+string='I love you'
+s1=string[::-1]
+print(s1)
+
+"""
+"""
+
+a='I'
+b='love'
+c='you'
+d='so '
+e='much'
+f=f'{a} {b} {c} {d*100} {e}'
+print(f)
+
+"""
+"""
+
+a='I'
+b='love'
+c='you'
+d='so '
+e='much'
+
+import random
+x=random.randint(1,100)
+
+print(a,b,c,d*x,e)
+
+"""
